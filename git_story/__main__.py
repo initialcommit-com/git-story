b8b3803ba2b56825b7066e27301d901ccf3fdea9
Jacob Stopak <jacob@initialcommit.io>
2022-05-06 15:38:38 -0400

    Develop first functional version, run with: python3 __main__.py <number_of_commits>

---
+++ git_story/__main__.py
@@ -0,0 +1,12 @@
+import git_story as gs
+import sys
+from manim.utils.file_ops import open_file as open_media_file
+
+def main():
+    print(sys.argv[1])
+    scene = gs.GitStory(int(sys.argv[1]))
+    scene.render()
+    open_media_file(scene.renderer.file_writer.movie_file_path)
+
+if __name__ == '__main__':
+    main()
--- git_story/git_story.py
+++ git_story/git_story.py
@@ -2,6 +2,68 @@ from manim import *
 import git
 
 class GitStory(Scene):
+    def __init__(self, numberOfCommits):
+        super().__init__()
+        self.numberOfCommits = numberOfCommits
+
     def construct(self):
         repo = git.Repo(search_parent_directories=True)
         commits = list(repo.iter_commits('HEAD'))
+        commits.reverse()
+
+        initialCommitLogo = ImageMobject("logo.png")
+        self.add(initialCommitLogo)
+
+        initialCommitText = Text("Git Story, by initialcommit.com", font="Monospace", font_size=36).to_edge(UP, buff=1)
+        self.add(initialCommitText)
+        self.wait(2)
+        self.play(FadeOut(initialCommitText))
+        self.play(initialCommitLogo.animate.scale(0.25).to_edge(UP, buff=0).to_edge(RIGHT, buff=0))
+
+        i = 1
+        prevCircle = None
+        toFadeOut = Group()
+        for commit in commits[:self.numberOfCommits]:
+            circle = Circle()
+            circle.height = 1
+
+            if prevCircle:
+                circle.next_to(prevCircle, RIGHT, buff=1.5)
+            else:
+                circle.to_edge(LEFT)
+
+            arrow = Arrow(start=RIGHT, end=LEFT).next_to(circle, LEFT, buff=0)
+            arrow.width = 1
+
+            commitId = Text(commit.hexsha[0:6], font="Monospace", font_size=20).next_to(circle, UP)
+
+            threeDots = ''
+            if ( len(commit.message) > 18 ):
+                threeDots = '...'
+
+            message = Text(commit.message[:18] + threeDots, font="Monospace", font_size=14).next_to(circle, DOWN)
+
+            self.play(Create(circle), AddTextLetterByLetter(commitId), AddTextLetterByLetter(message))
+
+            if ( prevCircle ):
+                self.play(Create(arrow))
+                toFadeOut.add(arrow)
+
+            prevCircle = circle
+
+            toFadeOut.add(circle, commitId, message)
+            i += 1
+
+        self.wait(3)
+
+        self.play(FadeOut(toFadeOut))
+
+        self.play(initialCommitLogo.animate.scale(4).set_x(0).set_y(0))
+
+        thankYouText= Text("Thanks for using Initial Commit!", font="Monospace", font_size=36).to_edge(UP, buff=1)
+        self.play(AddTextLetterByLetter(thankYouText))
+
+        learnMoreText = Text("Learn more at initialcommit.com", font="Monospace", font_size=36).to_edge(DOWN, buff=1)
+        self.play(AddTextLetterByLetter(learnMoreText))
+
+        self.wait(3)
